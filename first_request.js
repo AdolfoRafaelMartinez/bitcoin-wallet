388b74c643a435c6527d803be945763a7e7a7316
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2025-12-12 17:44:18 -0600

    first request using fetch

---
+++ first_request.js
@@ -0,0 +1,25 @@
+import { Core } from '@quicknode/sdk'
+
+const QUICKNODE_URL = 'https://wispy-muddy-mound.btc-testnet4.quiknode.pro/9d3168def96c68f2c77df93184521a4ac1aa661f/';
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+    "method": "eth_blockNumber",
+    "params": [],
+    "id": 1,
+    "jsonrpc": "2.0"
+});
+
+var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+fetch(QUICKNODE_URL, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
